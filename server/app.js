0061c856a1f563e67fe35bd2a2d081e4f9165082
leoni <leonimega@hotmail.com>
2019-01-27 14:56:57 -0200

    get directory from config file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
+process.env.NODE_CONFIG_DIR = 'server/config';
 const chokidar = require('chokidar');
 const config = require('config');
 
 const dirToWatch = config.get('watchDirectory');
 
 // One-liner for current directory, ignores .dotfiles
-chokidar.watch('.', { ignored: /(^|[/\\])\../ }).on('all', (event, path) => {
+chokidar.watch(dirToWatch, { ignored: /(^|[/\\])\../ }).on('all', (event, path) => {
   console.log(event, path);
 });
