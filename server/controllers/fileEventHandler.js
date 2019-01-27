efcaf23facc5497af7cc1367597d14a81e8f648e
leoni <leonimega@hotmail.com>
2019-01-27 16:05:34 -0200

    improve file handler controller

--- server/controllers/fileEventHandler.js
+++ server/controllers/fileEventHandler.js
@@ -4,31 +4,48 @@ const fs = require('fs');
 const logger = log4js.getLogger('fileEventHandler');
 logger.level = 'debug';
 
+const logFileStats = (meta) => {
+  // atime: last access time
+  // mtime: last modification time
+  // ctime: creation time
+  logger.info('=======================================================================================');
+  logger.info(`Path: ${meta.path}`);
+  logger.info(`Name: ${meta.name}`);
+  logger.info(`Creation datetime: ${meta.creationDate}`);
+  logger.info(`Last modification datetime: ${meta.modificationDate}`);
+  logger.info(`Size in bytes: ${meta.size}`);
+  logger.info('=======================================================================================');
+};
+
+const getFileMeta = path => new Promise((resolve, reject) => {
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      logger.error(`Error occured while reading stats for file: ${path}`);
+      logger.error(err);
+      reject(err);
+    }
+
+    if (stats.isFile()) {
+      resolve({
+        name: path.replace(/^.*[\\/]/, ''),
+        path,
+        creationDate: stats.ctime,
+        modificationDate: stats.ctime,
+        size: stats.size,
+      });
+    }
+    reject(new Error('test'));
+  });
+});
+
 module.exports = {
   handleAdd: (path) => {
     logger.debug(`Handling add for: ${path}`);
   },
   handleChange: (path) => {
     logger.debug(`Handling change for: ${path}`);
-    fs.stat(path, (err, stats) => {
-      if (err) {
-        logger.error(`Error occured while reading stats for file: ${path}`);
-        logger.error(err);
-        return;
-      }
-      // atime: last access time
-      // mtime: last modification time
-      // ctime: creation time
-      if (stats.isFile()) {
-        logger.info('=======================================================================================');
-        logger.info(`Path: ${path}`);
-        logger.info(`Name: ${path.replace(/^.*[\\/]/, '')}`);
-        logger.info(`Creation datetime: ${stats.ctime}`);
-        logger.info(`Last modification datetime: ${stats.mtime}`);
-        logger.info(`Size in bytes: ${stats.size}`);
-        logger.debug(JSON.stringify(stats));
-        logger.info('=======================================================================================');
-      }
+    getFileMeta(path).then((meta) => {
+      logFileStats(meta);
     });
   },
   handleUnlink: (path) => {
